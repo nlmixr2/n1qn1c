9474adf53ec1cadabb105693c0c68059fad4bd05
Matthew Fidler <matthew.fidler@gmail.com>
2024-08-21 08:47:38 -0500

    Add ptrs function

--- src/init.c
+++ src/init.c
@@ -6,17 +6,17 @@
 #include "n1qn1.h"
 
 extern void n1qn1_(S2_fp simul, int n[], double x[], double f[], double g[], double var[], double eps[],
-		   int mode[], int niter[], int nsim[], int imp[], double zm[], int izs[], 
+		   int mode[], int niter[], int nsim[], int imp[], double zm[], int izs[],
 		   float rzs[], double dzs[], int id[]);
 void n1qn1F(S2_fp simul, int n[], double x[], double f[], double g[], double var[], double eps[],
-		   int mode[], int niter[], int nsim[], int imp[], int lp[], double zm[], int izs[], 
+		   int mode[], int niter[], int nsim[], int imp[], int lp[], double zm[], int izs[],
 		   float rzs[], double dzs[]) {
   int id = 0;
   n1qn1_(simul, n, x, f, g, var, eps, mode, niter, nsim, imp, zm, izs, rzs, dzs, &id);
 }
 
 void n1qn1F2(S2_fp simul, int n[], double x[], double f[], double g[], double var[], double eps[],
-		   int mode[], int niter[], int nsim[], int imp[], double zm[], int izs[], 
+		   int mode[], int niter[], int nsim[], int imp[], double zm[], int izs[],
 		   float rzs[], double dzs[]) {
   int id = 0;
   n1qn1_(simul, n, x, f, g, var, eps, mode, niter, nsim, imp, zm, izs, rzs, dzs, &id);
@@ -24,9 +24,47 @@ void n1qn1F2(S2_fp simul, int n[], double x[], double f[], double g[], double va
 /* .C calls */
 extern SEXP n1qn1_wrap(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
+SEXP _n1qn1_ptr(void) {
+  int pro = 0;  // Counter for the number of PROTECT calls
+
+  // Create an external pointer
+  SEXP n1qn1c_n1qn1F = PROTECT(R_MakeExternalPtrFn((DL_FUNC)&n1qn1F, R_NilValue, R_NilValue)); pro++;
+
+  SEXP n1qn1c_n1qn1F2 = PROTECT(R_MakeExternalPtrFn((DL_FUNC)&n1qn1F2, R_NilValue, R_NilValue)); pro++;
+
+  SEXP n1qn1c_n1qn1_ = PROTECT(R_MakeExternalPtrFn((DL_FUNC)&n1qn1_, R_NilValue, R_NilValue)); pro++;
+
+#define nVec 48
+
+  SEXP ret = PROTECT(Rf_allocVector(VECSXP, nVec)); pro++;
+  SEXP retN = PROTECT(Rf_allocVector(STRSXP, nVec)); pro++;
+
+  SET_VECTOR_ELT(ret, 0, n1qn1c_n1qn1F);
+  SET_STRING_ELT(retN, 0, Rf_mkChar("n1qn1F"));
+
+  SET_VECTOR_ELT(ret, 1, n1qn1c_n1qn1F2);
+  SET_STRING_ELT(retN, 1, Rf_mkChar("n1qn1F2"));
+
+  SET_VECTOR_ELT(ret, 2, n1qn1c_n1qn1_);
+  SET_STRING_ELT(retN, 2, Rf_mkChar("n1qn1_"));
+
+#undef nVec
+
+  // Set the names attribute of the list
+  Rf_setAttrib(ret, R_NamesSymbol, retN);
+
+  // Unprotect all protected objects
+  UNPROTECT(pro);
+
+  // Return the list of external pointers
+  return ret;
+
+}
+
 void R_init_n1qn1(DllInfo *dll)
 {
   R_CallMethodDef callMethods[]  = {
+    {"_n1qn1_ptr", (DL_FUNC) &_n1qn1_ptr, 0},
     {"n1qn1_wrap", (DL_FUNC) &n1qn1_wrap, 13},
     {NULL, NULL, 0}
   };
@@ -35,4 +73,4 @@ void R_init_n1qn1(DllInfo *dll)
   R_RegisterCCallable("n1qn1","n1qn1_", (DL_FUNC) &n1qn1_);
   R_registerRoutines(dll, NULL, callMethods, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
-} 
+}
